d0d312968ff37341d609daf3292732a2fea79214
wang_125309 <wang_125309@foxmail.com>
2015-04-24 07:57:13 +0800

    push

--- static/js-modify/fullScene.js
+++ static/js-modify/fullScene.js
@@ -16,33 +16,19 @@ window.onload = function() {
     var deviceMotionHandler = function(eventData) {
         console.log(eventData);
         xm = Math.round(eventData.alpha*10)/10;
-        x = Math.round(6*h/360*xm+(1608+w/2));
-        if(lx > 358&& xm<2) {
-            count ++;
-        }
-        else if(lx < 2&& xm>358) {
-            count --;
-        }
-        lx = xm;
-        z = Math.round(3*h/180*Math.round(eventData.beta*10)/10+h);
-        y = Math.round(6*h/180*Math.round(eventData.gamma*10)/10);
-        x = count*3216+x;
-        if(z>643&&z<=1608) {
+        x = Math.round(2*h/180*xm+(2*h+w/2));
+        xm -= 180;
+        //z = Math.round(4*h/180*(Math.round(eventData.beta*10)/10-90));
+        //z = (Math.round(Math.round(eventData.beta*10))/10);
+        z = Math.round(Math.round(eventData.beta*10)/10/90*h+h);
+        y = Math.round(2*h/180*Math.round(eventData.gamma*10)/10);
+        if(Math.abs((x+y)-(lx+ly))>3||Math.abs((z)-(lz))>3) {
             $(".background").css({
                 "background-position":x+y+"px "+z+"px"
             });
-        }
-        else if(z<643){
-            $(".background").css({
-                "background-position":x+y+"px "+643+"px"
-            });
-        
-        }
-        else if(z>1608){
-            $(".background").css({
-                "background-position":x+y+"px "+1608+"px"
-            });
-        
+            lx = x;
+            ly = y;
+            lz = z;
         }
     };
     var u = navigator.userAgent;
@@ -56,52 +42,53 @@ window.onload = function() {
         },3000);
     }
     else {
-    x = 0;
-    y = 0;
-    mx = 1608+w/2;
-    my = 2*h;
+        x = 0;
+        y = 0;
+        mx = 2*h+w/2;
+        my = 2*h;
         $(".move").addClass("move-android");
         $(".move-text").html("清扫观赏全景");
         setTimeout(function(){
             $(".move").velocity("fadeOut");
             $(".move-text").velocity("fadeOut");
         },3000);
-    $(".background").on("touchstart",function(e){
-        x = e.touches[0].clientX;
-        y = e.touches[0].clientY;
-        
-    });
-    $(".background").on("touchend",function(e){
-        x = e.touches[0].clientX;
-        y = e.touches[0].clientY;
-        
-    });
-    $(".background").on("touchmove",function(e){
-        console.log(e.touches[0]);
-        e.preventDefault();
-        setTimeout(function(){
-            mx += e.touches[0].clientX - x-2;
+        document.addEventListener('touchmove', function (event) {
+        event.preventDefault();
+    }, false);
+        $(".background").on("touchstart",function(e){
             x = e.touches[0].clientX;
-            my += e.touches[0].clientY-y-2;
             y = e.touches[0].clientY;
-            if(my>643&&my<1608) {
-                $(".background").css({
-                "background-position":mx+"px "+my+"px"
-                });
-            }
-            else if(my<643){
-            
-                $(".background").css({
-                "background-position":mx+"px "+643+"px"
-                });
-            }
-            else if(my>1608) {
-                $(".background").css({
-                "background-position":mx+"px "+1608+"px"
-                });
-                
-            }
-        },5);
-    });
+        });
+        $(".background").on("touchend",function(e){
+            x = e.touches[0].clientX;
+            y = e.touches[0].clientY;
+        });
+        $(".background").on("touchmove",function(e){
+                console.log(e.touches[0]);
+                e.preventDefault();
+                setTimeout(function(){
+                    mx += e.touches[0].clientX - x-2;
+                    x = e.touches[0].clientX;
+                    my += e.touches[0].clientY-y-2;
+                    y = e.touches[0].clientY;
+                    if(my>h&&my<2*h) {
+                    $(".background").css({
+                        "background-position":mx+"px "+my+"px"
+                        });
+                    }
+                    else if(my<h){
+
+                    $(".background").css({
+                        "background-position":mx+"px "+h+"px"
+                        });
+                    }
+                    else if(my>2*h) {
+                    $(".background").css({
+                        "background-position":mx+"px "+2*h+"px"
+                        });
+
+                    }
+                },5);
+        });
     }
 };
